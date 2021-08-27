de0769a993f3ab8c9cd84678a5b27df5ba7f4407
Jaqueline Fogaça <parkjaque1995@gmail.com>
2021-08-26 21:55:34 -0300

    Adicionar arquivos de projeto.

---
+++ Dados Pessoais/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dados_Pessoais
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			pessoa A;
+			A = new pessoa();
+			pessoa B;
+			B = new pessoa();
+
+			Console.WriteLine("Digite o nome da primeira pessoa:");
+			A.Nome = Console.ReadLine();
+			Console.WriteLine("Digite a idade da primeira pessoa:");
+			A.Idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			Console.WriteLine("Digite o nome da segunda pessoa:");
+			B.Nome = Console.ReadLine();
+			Console.WriteLine("Digite a idade da segunda pessoa:");
+			B.Idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+			if (A.Idade > B.Idade)
+			{
+				Console.WriteLine("A pessoa mais velha é: " + A.Nome);
+			}
+			else if (A.Idade == B.Idade)
+			{
+				Console.WriteLine("As duas pessoas tem a mesma idade");
+
+			}
+			else
+			{
+				Console.WriteLine("A pessoa mais velha é: " + B.Nome);
+			}
+				
+
+		
+
+		}
+	}
+}
---
+++ Dados Pessoais/pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dados_Pessoais
+{
+	class pessoa
+	{
+		public string Nome;
+		public int Idade;
+
+	}
+}
